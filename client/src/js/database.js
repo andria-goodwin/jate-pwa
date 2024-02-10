371997e14b722a176b59b32d59f1a970a0ee47af
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2024-02-09 18:32:43 -0600

    added put method to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,18 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// method that accepts some content and adds it to the IndexedBD database using the idb module
+export const putDb = async (content) => {
+  console.log('PUT to the database');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('New data saved to the database', result.value);
+}
 
-// method that gets all the content from the  IndexedDB database using the idb module
+// method that gets all the content from the IndexedDB database using the idb module
 export const getDb = async () => {
   console.log('GET from the database');
   const jateDb = await openDB('jate', 1);
